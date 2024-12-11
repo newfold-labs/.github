01fbf26c2783610fb534bf1c9afc4264c715403c
Francesco Licandro <francesco.licandro@yithemes.com>
2024-12-11 17:21:47 +0100

    new import rulesets script

---
+++ scripts/import-rulesets.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+RULESETS_PATH='../rulesets/'
+PAGE=1
+
+# Get command-line arguments
+owner="$1"
+
+if [ ! $owner ]; then
+    echo -e "${RED}Please specify a repo owner: importRulesets.sh <owner>${NOCOLOR}"
+    exit 1;
+fi
+
+while true; do
+    repos=$(gh api "users/$owner/repos?per_page=20&page=$PAGE" --jq '.[].name') 
+    if [ -z "$repos" ]; then 
+        break 
+    fi 
+    
+    for repo in $repos; do 
+        echo "Processing repository: $repo"
+
+        rulesets=$(gh api /repos/$owner/$repo/rulesets )
+
+        for file in "$RULESETS_PATH"/*.json; do
+            if [ -f "$file" ]; then
+                
+                name=$(jq -c '.name' "$file")
+                id=$( echo "$rulesets" | jq ".[] | select(.name == $name) | .id" )
+
+                if [ -z "$id" ]; then
+                    gh api --method POST -H "Accept: application/vnd.github+json" /repos/$owner/$repo/rulesets --input "$file" --silent
+                else
+                    gh api --method PUT -H "Accept: application/vnd.github+json" /repos/$owner/$repo/rulesets/$id --input "$file" --silent
+                fi
+            fi
+        done
+    done
+
+    PAGE=$((PAGE + 1))
+done
